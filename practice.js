eb1a7284ef71ae1b3fa193858ad4156af4610fa5
Badal Chandra Halder <badalchandrahalder99@gmail.com>
2026-02-02 23:42:38 +0600

    Refactor function to reverse an array in place and update console output

--- practice.js
+++ practice.js
@@ -1,28 +1,27 @@
-// - Find the largest and smallest number from an array ?
+// - Reverse an array in place?
 
 const numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
 
-const findLargest = (arr) => {
+const reverseArr = (arr) => {
 
-   let max = arr[0]
-   let min = arr[0]
+   let reversed = []
 
-   for (let number of arr) {
-      if (number > max) {
-         max = number
-      } else if (number < min) {
-         min = number
-      }
+   for (let i = arr.length - 1; i < arr.length; i--) {
+      const current = arr[i]
+      reversed.push(current)
    }
 
-   return `The max number is ${max} and The min number is ${min}`
+   return reversed
 }
 
-const output = findLargest(numbers)
-console.log(`Ouput is ${output}`)
+const output = reverseArr(numbers)
+console.log(output)
 
 
-
+// Solution - 02:
+const arr = [1, 2, 3, 4, 5];
+arr.reverse();
+console.log(arr); // [5, 4, 3, 2, 1]
 
 
 
